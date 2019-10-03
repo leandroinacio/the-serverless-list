38472a9b749bcc47bee38823dff0577e62b781e9
Leandro <le.inacio.souza@gmail.com>
2019-10-03 14:46:30 -0300

    Created first mock and functions to view and fetch reports

--- handler.js
+++ handler.js
@@ -1,29 +1,39 @@
 'use strict';
 
+// Mocked news
+let reports = [{
+		id: 1,
+		title: 'Spider-man is a Menace!',
+		views: 1
+	},
+	{
+		id: 2,
+		title: 'Los pollos hermanos',
+		views: 3
+	},
+	{
+		id: 3,
+		title: 'Weather today',
+		views: 2
+	}
+];
+
+const orderByViews = (reportA, reportB) => reportB.views - reportA.views;
+
+// Fetch all news ordered by views
 module.exports.fetch = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'FETCH The Daily CY&D is up and running!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };  
+	return {
+		statusCode: 200,
+		body: JSON.stringify(reports.sort(orderByViews)),
+	};
 };
 
-module.exports.view = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'VIEWThe Daily CY&D is up and running!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };  
+// Ping report to update view count
+module.exports.view = async (event, context, callback) => {
+	const report = reports.find((current) => current.id == event.pathParameters.id);
+	report.views++;
+	return {
+		statusCode: 200,
+		body: JSON.stringify(report)
+	};
 };
